f328f9b94b1e77a8b2ac6e90a6b0436542663518
vimesa <vimesa@redborder.com>
2022-08-08 09:33:18 +0100

    Add files

---
+++ lib/logstash/filters/util/yara_constant.rb
@@ -0,0 +1,3 @@
+module YaraConstant
+
+end
---
+++ lib/logstash/filters/yara.rb
@@ -0,0 +1,49 @@
+# encoding: utf-8
+require "logstash/filters/base"
+require "logstash/namespace"
+require 'json'
+require 'time'
+
+require_relative "util/yara_constant"
+
+class LogStash::Filters::Yara < LogStash::Filters::Base
+  include YaraConstant
+
+  config_name "yara"
+
+  config :yara_rule,    :validate => :string,   :default => "/opt/rb/var/yara_rules/rules.yara"
+  config :yara_path,    :validate => :string,   :default => "/opt/rb/bin/yara"
+  config :file,         :validate => :string,   :default => "[path]"
+  #config :target,       :validate => :string,   :default => "yara"
+  config :score_name,   :validate => :string,   :default => "fb_yara"
+  config :weight,       :default => 1.0
+
+  DELAYED_REALTIME_TIME = 15
+
+  public
+  def register
+    # Add instance variables
+
+  end # def register
+
+  public
+
+  def filter(event)
+
+    @path = event.get(@file)
+
+    starting_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+
+    yara_score = `@yara_path -c -w @yara_rule @path`.to_i
+
+    ending_time  = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+    elapsed_time = (ending_time - starting_time).round(1)
+    event.set('yara_latency', elapsed_time)
+
+    #event.set(@target,)
+    event.set(@score_name,yara_score)
+
+    filter_matched(event)
+
+  end  # def filter(event)
+end # class LogStash::Filters::Yara
