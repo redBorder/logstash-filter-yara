34bec8bc00eb22158b5d2b1bc807dd7e7bcdc862
vimesa <vimesa@redborder.com>
2022-08-08 12:48:33 +0100

    Added target name

--- lib/logstash/filters/yara.rb
+++ lib/logstash/filters/yara.rb
@@ -14,11 +14,10 @@ class LogStash::Filters::Yara < LogStash::Filters::Base
   config :yara_rule,    :validate => :string,   :default => "/opt/rb/var/yara_rules/rules.yara"
   config :yara_path,    :validate => :string,   :default => "/opt/rb/bin/yara"
   config :file,         :validate => :string,   :default => "[path]"
-  #config :target,       :validate => :string,   :default => "yara"
   config :score_name,   :validate => :string,   :default => "fb_yara"
   config :weight,       :default => 1.0
   config :latency_name, :validate => :string,   :default => "yara_latency"
-
+  config :target, :validate => :string, :default => "yara"
 
   DELAYED_REALTIME_TIME = 15
 
@@ -47,9 +46,8 @@ class LogStash::Filters::Yara < LogStash::Filters::Base
     ending_time  = Process.clock_gettime(Process::CLOCK_MONOTONIC)
     elapsed_time = (ending_time - starting_time).round(1)
     event.set(@latency_name, elapsed_time)
-    event.set('detected_rules',det_rules)
+    event.set(@target,det_rules)
 
-    #event.set(@target,)
     event.set(@score_name,yara_score)
 
     filter_matched(event)
