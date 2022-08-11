a95f1de2fd4a1a003fffaff27809cce0f9692538
vimesa <vimesa@redborder.com>
2022-08-11 10:59:37 +0100

    Modify plugin yara

--- lib/logstash/filters/yara.rb
+++ lib/logstash/filters/yara.rb
@@ -12,7 +12,7 @@ class LogStash::Filters::Yara < LogStash::Filters::Base
   config_name "yara"
 
   config :yara_rule,    :validate => :string,   :default => "/opt/rb/var/yara_rules/rules.yara"
-  config :yara_path,    :validate => :string,   :default => "/opt/rb/bin/yara"
+  config :yara_bin,    :validate => :string,   :default => "/opt/rb/bin/yara"
   config :file,         :validate => :string,   :default => "[path]"
   config :score_name,   :validate => :string,   :default => "fb_yara"
   config :weight,       :default => 1.0
@@ -27,6 +27,40 @@ class LogStash::Filters::Yara < LogStash::Filters::Base
 
   end # def register
 
+  private
+
+  def get_yara_info
+
+    yara_score = -1
+    det_rules = []
+    yara_json={}
+
+    unless File.exist?(@yara_bin)
+      @logger.error("Yara binary is not in #{@yara_bin}.")
+      return [yara_json, yara_score]
+    end
+
+    unless File.exist?(@path)
+      @logger.error("File #{@path} does not exist.")
+      return [yara_json, yara_score]
+    end
+
+    unless File.exist?(@yara_rule)
+      @logger.error("Yara rule is not in #{@yara_rule}.")
+      return [yara_json, yara_score]
+    end
+
+    yara_result = `#{@yara_bin} -w #{@yara_rule} #{@path}`
+
+    det_rules = yara_result.split("\n").map! { |rule| rule.split(" ").first } if !yara_result.empty?
+    yara_json = {
+      "detected_rules" => det_rules
+    }
+    yara_score = (det_rules.length * @weight).round
+
+    [yara_json, yara_score]
+  end
+
   public
 
   def filter(event)
@@ -34,20 +68,13 @@ class LogStash::Filters::Yara < LogStash::Filters::Base
     @path = event.get(@file)
 
     starting_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-
-    det_rules = []
-    yara_result = `#{@yara_path} -w #{@yara_rule} #{@path}`
-
-    det_rules = yara_result.split("\n").map! { |rule| rule.split(" ").first } if !yara_result.empty?
-
-    yara_score = (det_rules.length * @weight).round
-
+    yara_info,yara_score = get_yara_info
 
     ending_time  = Process.clock_gettime(Process::CLOCK_MONOTONIC)
     elapsed_time = (ending_time - starting_time).round(1)
-    event.set(@latency_name, elapsed_time)
-    event.set(@target,det_rules)
 
+    event.set(@latency_name, elapsed_time)
+    event.set(@target,yara_info)
     event.set(@score_name,yara_score)
 
     filter_matched(event)
