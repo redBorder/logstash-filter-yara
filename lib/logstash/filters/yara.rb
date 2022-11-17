5eb0a8397f41a5aec0933064d90058436d3984b6
vimesa <vimesa@redborder.com>
2022-11-17 10:42:52 +0000

    Modify yara.rb

--- lib/logstash/filters/yara.rb
+++ lib/logstash/filters/yara.rb
@@ -19,25 +19,23 @@ class LogStash::Filters::Yara < LogStash::Filters::Base
   config_name "yara"
 
   # Python path
-  config :python,                   :validate => :string,         :default => "/usr/bin/python2.6"
+  config :python,                     :validate => :string,         :default => "/usr/bin/python2.6"
   # File that is going to be analyzed
-  config :file,                     :validate => :string,         :default => "[path]"
+  config :file_field,                 :validate => :string,         :default => "[path]"
   # Where you want the score to be placed
-  config :score_name,               :validate => :string,         :default => "fb_yara"
+  config :score_name,                 :validate => :string,         :default => "fb_yara"
   # Where you want the latency to be placed
-  config :latency_name,             :validate => :string,         :default => "yara_latency"
+  config :latency_name,               :validate => :string,         :default => "yara_latency"
   # Where you want the data to be placed
-  config :target,                   :validate => :string,         :default => "yara"
+  config :target,                     :validate => :string,         :default => "yara"
   # Pyyara python script path
-  config :script,                   :validate => :string,         :default => "/opt/rb/var/yara/scripts/pyyara.py"
-  # Path of yara - Test
-  #config :dir,                      :validate => :string,         :default => "/opt/rb/var/yara"
+  config :pyyara_py,                  :validate => :string,         :default => "/opt/rb/var/rb-sequence-oozie/workflow/lib/scripts/pyyara.py"
   # Path of yara_weights
-  config :yara_weights,             :validate => :string,         :default => "/opt/rb/var/yara/yara_loader.yml"
+  config :yara_weights,               :validate => :string,         :default => "/opt/rb/var/rb-sequence-oozie/workflow/yara_loader.yml"
   # Aerospike server in the form "host:port"
-  config :aerospike_server,         :validate => :string,         :default => ""
+  config :aerospike_server,           :validate => :string,         :default => ""
   # Namespace is a Database name in Aerospike
-  config :aerospike_namespace,      :validate => :string,         :default => "malware"
+  config :aerospike_namespace,        :validate => :string,         :default => "malware"
 
 
   DELAYED_REALTIME_TIME = 15
@@ -61,49 +59,127 @@ class LogStash::Filters::Yara < LogStash::Filters::Base
   def get_yara_info
 
     yara_score = -1
-    det_rules = []
-    yara_json={}
+    yara_info = {}
 
-    unless File.exist?(@path)
-      @logger.error("File #{@path} does not exist.")
-      return [yara_json, yara_score]
-    end
 
-    unless File.exist?(@script)
-      @logger.error("Script Yara - pyyara.py - is not in #{@script}.")
-      return [yara_json, yara_score]
-    end
+    # exceptions - start
 
-    unless File.exist?(@dir)
-      @logger.error("Path #{@dir} does not exist.")
-      return [yara_json, yara_score]
+    unless File.exist?(@pyyara_py)
+      @logger.error("Script Yara - pyyara.py - is not in #{@pyyara_py}.")
+      return [yara_info, yara_score]
     end
 
     unless File.exist?(@yara_weights)
       @logger.error("File #{@yara_weights} does not exist.")
-      return [yara_json, yara_score]
+      return [yara_info, yara_score]
     end
 
     unless File.exist?(@python)
       @logger.error("Python is not in #{@python}.")
-      return [yara_json, yara_score]
+      return [yara_info, yara_score]
     end
 
-    begin
-      yara_json = JSON.parse(`#{@python} #{@script} #{@file}`)
-    rescue JSON::ParseError
-      @logger.error("Cannot get score from #{@file}")
+    # exceptions - end
+
+
+    hits={}
+    scores={}
+
+    command = `#{@pyyara_py} #{@file_field}`
+    result = `#{command}`
+
+    #HIGH_SEVERITY = "high"
+    #MEDIUM_SEVERITY = "medium"
+    #LOW_SEVERITY = "low"
+
+
+    high_severity = "high"
+    medium_severity = "medium"
+    low_severity = "low"
+
+    severities = [high_severity, medium_severity, low_severity]
+
+    severities.each do |severity|
+      hits[severity] = 0
+      scores[severity] = 0
+    end
+
+    py_json = JSON.parse(result)
+    matches = py_json["matches"]
+
+    severity = ""
+    rule = ""
+
+    matches.each do |n|
+
+      meta = n["meta"]
+      severity = n["meta"]["severity"]
+      rule = n["rule"]
+      #severity = nil
+
+
+      if severity.nil?
+        hits[low_severity] += 1
+        #hits[severity] = 0
+        next
+      end
+
+      case severity.downcase
+      when high_severity.downcase
+        hits[severity] += 1
+      when medium_severity.downcase
+        hits[severity] += 1
+      when low_severity.downcase
+        hits[severity] += 1
+      end
+    end
+
+    yara_score = matches.length()
+
+    final_score = 0.0
+
+    sev = YAML.load_file("#{@yara_weights}")
+
+    general = sev["general"]
+    high = sev["high"]
+    medium = sev["medium"]
+    low = sev["low"]
+
+    if (!(hits[high_severity] == 0 and hits[medium_severity] == 0 and hits[low_severity] == 0))
+
+      severities.each do |severity|
+
+        if hits[severity] > sev[severity]["high_lower_threshold"]
+          scores[severity] = sev[severity]["high_score"]
+        elsif (hits[severity] < sev[severity]["low_upper_threshold"] and hits[severity] > 0)
+          scores[severity] = sev[severity]["low_score"]
+        elsif hits[severity] > 0
+          scores[severity] = sev[severity]["medium_score"]
+        end
+      end
     end
 
 
+    severities.each do |severity|
+      weighing = sev["general"][severity]
+      score = scores[severity]
+      final_score += weighing * score
+    end
 
-    yara_result = `#{@yara_bin} -w #{@yara_rule} #{@path}`
+    rules = []
+    matches.each do |n|
+      rule = n["rule"]
+      rules<<rule
+    end
 
-    det_rules = yara_result.split("\n").map! { |rule| rule.split(" ").first } if !yara_result.empty?
+
     yara_json = {
-      "detected_rules" => det_rules
+      "Rules" => rules,
+      "Score" => final_score,
+      "File" => @file_field,
+      "Hits" => hits,
+      "Match" => matches
     }
-    yara_score = (det_rules.length * @weight).round
 
     [yara_json, yara_score]
   end
@@ -112,18 +188,20 @@ class LogStash::Filters::Yara < LogStash::Filters::Base
 
   def filter(event)
 
-    @path = event.get(@file)
+    @path = event.get(@file_field)
 
     starting_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-    yara_info,yara_score = get_yara_info
+    yara_result,yara_score = get_yara_info
 
     ending_time  = Process.clock_gettime(Process::CLOCK_MONOTONIC)
     elapsed_time = (ending_time - starting_time).round(1)
 
     event.set(@latency_name, elapsed_time)
-    event.set(@target,yara_info)
+    event.set(@target,yara_result)
     event.set(@score_name,yara_score)
 
+
+    # filter_matched should go in the last line of our successful code
     filter_matched(event)
 
   end  # def filter(event)
